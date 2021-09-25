3939fc7eb8794480a3542a6fef9b294cd99c9561
goldencar0822 <>
2021-09-25 12:37:17 -0400

    Updates

---
+++ precache-manifest.a3b51b7455c5103764c1ec3fafe105f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f36ea4b92acd615773",
+    "url": "/econcircles/static/css/main.6ff47971.chunk.css"
+  },
+  {
+    "revision": "46f36ea4b92acd615773",
+    "url": "/econcircles/static/js/main.f768cfd7.chunk.js"
+  },
+  {
+    "revision": "c833be84220dc3bd9986",
+    "url": "/econcircles/static/js/runtime~main.3dabcb71.js"
+  },
+  {
+    "revision": "219c53993d795c7decaa",
+    "url": "/econcircles/static/js/2.20cf1f43.chunk.js"
+  },
+  {
+    "revision": "407072ee6ea55a5c2a30bc0ef8a56589",
+    "url": "/econcircles/index.html"
+  }
+];
